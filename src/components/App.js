4764ea59556061fa5997f554772ba83633ddd2b7
amenyona <amenyona.late@gmail.com>
2021-08-09 15:11:11 +0200

    init

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import logo from '../assets/logo.png'
+import Banniere from './Banniere'
+import PresidentList from './PresidentList'
+import Footer from './Footer'
+
+function App() {
+	return (
+		<div>
+			{/*<Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>*/}
+			<Banniere>
+				<img src={logo} alt='La maison jungle' className='lmj-logo'/>
+				<h1 className='lmj-title'>La liste des présidents</h1>
+			</Banniere>
+			{/* <Cart /><ShoppingList /> */}
+			<PresidentList />
+			<Footer />
+			
+		</div>
+	)
+}
+
+export default App
